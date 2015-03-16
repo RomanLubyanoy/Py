dd48c4445450be648ab48060099710188b2a951a
RomanLubyanoy <toproxi@gmail.com>
2015-03-16 17:50:19 +0200

    upd

--- PromPyCours/lesson8_4.py
+++ PromPyCours/lesson8_4.py
@@ -21,10 +21,79 @@
 
 Приклад вхідних і вихідних даних:
 print make_sudoku(1) # [[1]]
-print make_sudoku(2) # [[1,2,3,4],[3,4,1,2],[2,1,4,3],[4,3,2,1]]
-print make_sudoku(3) # [[3,5,8,1,2,7,6,4,9],[6,7,4,5,8,9,3,2,1],[2,1,9,3,4,6,5,7,8],[9,8,6,7,1,4,2,5,3],[4,3,1,2,6,5,8,9,7],[7,2,5,9,3,8,1,6,4],[1,6,3,4,7,2,9,8,5],[8,9,7,6,5,1,4,3,2],[5,4,2,8,9,3,7,1,6]]
+print make_sudoku(2) # [[1,2,3,4],
+                        [3,4,1,2],
+                        [2,1,4,3],
+                        [4,3,2,1]]
+print make_sudoku(3) # [[3,5,8,1,2,7,6,4,9],
+                        [6,7,4,5,8,9,3,2,1],
+                        [2,1,9,3,4,6,5,7,8],
+                        [9,8,6,7,1,4,2,5,3],
+                        [4,3,1,2,6,5,8,9,7],
+                        [7,2,5,9,3,8,1,6,4],
+                        [1,6,3,4,7,2,9,8,5],
+                        [8,9,7,6,5,1,4,3,2],
+                        [5,4,2,8,9,3,7,1,6]]
 """
-def make_sudoku(size):
-    return 0
+from random import shuffle
 
-print make_sudoku(1) # [[1]]
+
+def make_sudoku(size):
+    vals = [i for i in range(1, size ** 2 + 1)]
+    rez = [[0 for j in range(1, size ** 2 + 1)] for i in range(1, size ** 2 + 1)]
+    shuffle(vals)
+    r_value = vals[::]
+    c_value = vals[1:]
+    shuffle(c_value)
+    r_count = 0
+    c_count = 1
+
+    for s_size in range(len(rez)):
+
+        for ii in range(0, len(r_value)):
+            rez[r_count][s_size] = r_value[ii]
+        for jj in range(0, len(c_value)):
+                rez[jj + 1][c_count - 1] = c_value[jj]
+#        print r_value
+#        print c_value
+
+        r_value.pop()
+        r_count += 1
+
+    return rez
+"""
+    while count != 1:
+        rez = []
+        for i in range(size ** 2):
+            shuffle(vals)
+            rez.append(vals[::])
+        count = 0
+        for ii in range(size ** 2):
+            sud = set()
+            for jj in range(size ** 2):
+    #            print rez[jj][ii]
+                sud.add(rez[jj][ii])
+#            print len(sud)
+            if len(sud) == size ** 2:
+                count += 1
+            else:
+                break
+    return rez
+"""
+l = make_sudoku(3) # [[1]]
+#print l
+print '----------'
+for ii in range(len(l)):
+    print l[ii]
+#print set(l[0][0], l[1][0], l[2][0], l[3][0])
+
+
+"""
+l = [1, 2, 3, 4]
+shuffle(l)
+print l
+shuffle(l)
+print l
+shuffle(l)
+print l
+"""
